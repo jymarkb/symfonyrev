4fe500eda66b799bf52a45ba1115eda37fbc3150
arcobaleno <jymark.borja@gmail.com>
2025-02-08 12:45:33 +0800

    add: controller

---
+++ src/Controller/MainController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+final class MainController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index()
+    {
+        return $this->render('home/index.html.twig');
+    //     return $this->json([
+    //         'message' => 'Welcome to your new controller!',
+    //         'path' => 'src/Controller/MainController.php',
+    //     ]);
+    }
+
+    #[Route('/hello/{name?}', name: 'hello')]
+    public function helloUser(Request $request) {
+        $name = $request->get('name') ;
+        // return new Response('<h1> Hello ' .  $name .'!</h1>' );
+        return $this->render('home/hello.html.twig',
+        [
+            'name' => $name
+        ]);
+    }
+}
